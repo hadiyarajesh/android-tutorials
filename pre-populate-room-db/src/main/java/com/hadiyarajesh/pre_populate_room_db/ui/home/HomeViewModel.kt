c6a61e0f683560fdde7f3e6e6a3c55474705e469
hadiyarajesh <rajesh.hadiya@outlook.com>
2023-01-30 17:57:51 +0530

    Commented unnecessary code in pre-populate-room-db module

--- pre-populate-room-db/src/main/java/com/hadiyarajesh/pre_populate_room_db/ui/home/HomeViewModel.kt
+++ pre-populate-room-db/src/main/java/com/hadiyarajesh/pre_populate_room_db/ui/home/HomeViewModel.kt
@@ -1,28 +1,29 @@
 package com.hadiyarajesh.pre_populate_room_db.ui.home
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.hadiyarajesh.pre_populate_room_db.database.entity.User
 import com.hadiyarajesh.pre_populate_room_db.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val homeRepository: HomeRepository
 ) : ViewModel() {
-    init {
-        /**
-         * Initiate a fake DB read operation to (create and) initialise database on first app install.
-         * This will be executed every time when this viewmodel is instantiated, so it's not ideal for production
-         * Rather, use a datastore (or shared preference) to execute this operation only once in production.
-         */
-        viewModelScope.launch {
-            getUser()
-        }
-    }
+    /**
+     * If you're not using a [Flow] to read the database, this step is mandatory.
+     */
+//    init {
+//        /**
+//         * Initiate a fake DB read operation to (create and) initialise database on first app install.
+//         * This will be executed every time when this viewmodel is instantiated, so it's not ideal for production
+//         * Rather, use a datastore (or shared preference) to execute this operation only once in production.
+//         */
+//        viewModelScope.launch {
+//            getUser()
+//        }
+//    }
 
     private suspend fun getUser(): User? {
         return homeRepository.getFirstUser()
